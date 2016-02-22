53a930c0c5c0b1a5dc6cebc2b557e2c71e13e55b
Wen Bruce <cassiexia@gmail.com>
2016-02-21 22:00:52 -0500

    Initial file

---
+++ github_command.sh
@@ -0,0 +1,19 @@
+# Command for GITHUB
+# Add document
+git add file_name
+git commit -m "message"
+
+# Update all the document on GITHUB
+git push
+
+# Switch to a different branch
+git branch branck_name
+
+# Check out latest document
+git checkout branck_name
+
+# Clone git branch to local folder
+git clone https://github.com/cassiexia224/PythonLearning
+
+# Check current branch information
+git status
